347ae563133471b7d40d00e22a1275e39d34132f
Guglielmo Del Sarto <guglielmo@MacBook-Air-di-Guglielmo.local>
2021-06-27 00:03:32 +0100

    Final version

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Analytic"},{"l":"Bonds"},{"l":"ExamFilesBlocked"},{"l":"ExamFilesUser"},{"l":"LinearAlgebra"},{"l":"Options"},{"l":"Performance"},{"l":"RandomEnv"},{"l":"Stocks"},{"l":"Tests"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Analytic"},{"l":"Bonds"},{"l":"ExamFilesBlocked"},{"l":"ExamFilesUser"},{"l":"LinearAlgebra"},{"l":"Options"},{"l":"RandomEnv"},{"l":"Stocks"},{"l":"Tests"},{"l":"VAR"}]
